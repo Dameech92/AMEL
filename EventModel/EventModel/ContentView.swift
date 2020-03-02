3558eaf8949746b8c7e84ebab2a87db760a98ed6
Zins <richard.zins447@SIE-1222-19.local>
2020-03-02 09:39:43 -0800

    Created the event class. I also researched trying to add some more helper methods besides what was discussed, but the NSFetchrequest seems to handle almost all the functionality I could thin that we could need using predicates.

---
+++ EventModel/EventModel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EventModel
+//
+//  Created by Zins, Richard on 3/2/20.
+//  Copyright © 2020 Zins, Richard. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EventModel/EventModel/Event.swift
@@ -0,0 +1,28 @@
+//
+//  Event.swift
+//  EventModel
+//
+//  Created by Zins, Richard on 3/2/20.
+//  Copyright © 2020 Zins, Richard. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+public class Event: NSManagedObject, Identifiable {
+    @NSManaged public var  dateTime:Date?
+    @NSManaged public var name:String?
+    @NSManaged public var latitude:NSNumber?
+    @NSManaged public var longitude:NSNumber?
+    @NSManaged public var altitude:NSNumber?
+    @NSManaged public var heading:NSNumber?
+}
+
+extension Event {
+    static func getEvents() -> NSFetchRequest<Event> {
+        let request:NSFetchRequest<Event> = Event.fetchRequest() as! NSFetchRequest<Event>
+        let sortDescriptor = NSSortDescriptor(key: "createdAt", ascending: true)
+        request.sortDescriptors = [sortDescriptor]
+        return request
+    }
+}
