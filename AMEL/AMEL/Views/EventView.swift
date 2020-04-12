9be94fc7a146daedc6108dce7bba8f1d5764c8cf
neil-marcellini <neil.marcellini@gmail.com>
2020-04-12 14:14:09 -0700

    Fixed log view layout

--- AMEL/AMEL/Views/EventView.swift
+++ AMEL/AMEL/Views/EventView.swift
@@ -12,63 +12,47 @@ struct EventView: View{
     let event: Event
     var body: some View {
         let viewModel = EventFormatter(event: event)
-        return ZStack{
-                
+           return ZStack{
                 HStack{
-                    ZStack {
-                            
-                            RoundedRectangle(cornerRadius: 0)
-                               .fill(Color.blue)
-                                .frame(minHeight: 0, maxHeight: 150)
-                                .frame(minWidth: 0, maxWidth: 100)
-                        EventTitleBackground(name: viewModel.getName(), color: viewModel.getColor())
-                        .frame(minWidth:120, maxWidth: .infinity)
-                        .layoutPriority(1)
-
-                    }
-
-
+                    EventTitleBackground(name: viewModel.getName(), color: viewModel.getColor())
                     Spacer()
                     VStack(alignment: .leading){
+                        Spacer()
                         Text(viewModel.getTime())
-                            .font(.title)
+
                         Spacer()
                         Text(viewModel.getLatLng())
-                            .font(.title)
+
                         Spacer()
 
                         Text(viewModel.getAltitude())
-                            .font(.title)
+
+                        Spacer()
 
                     }
-                    .layoutPriority(4)
                     .padding()
                     Spacer()
                     VStack(alignment: .leading){
-                        HStack{
-                            Text(viewModel.getBoBR())
-                                .font(.title)
-                        }
+                        Spacer()
+                        Text(viewModel.getBoBR())
+
                         Spacer()
                         Text(viewModel.getHeadingCourse())
-                            .font(.title)
+
                         Spacer()
                         Text(viewModel.getGroundSpeed())
-                            .font(.title)
 
+                        Spacer()
                     }
-                    .layoutPriority(4)
                     .padding()
                     Spacer()
                 }
-            RoundedRectangle(cornerRadius: CGFloat(10))
-                .stroke(Color.black, lineWidth: 8)
-                .frame(minHeight: 0, maxHeight: .infinity)
+                .font(.title)
+                Rectangle()
+                .stroke(Color.black, lineWidth: 5)
+           }
+           .background(Color("buttonBackGround"))
         }
-        .background(Color("buttonBackGround"))
-        .frame(minHeight: 0, maxHeight: .infinity)
-
-    }
 }
 
 struct EventView_Previews: PreviewProvider {
