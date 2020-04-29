1428b34a593ec3b634338f61692233ae9be00b5d
Richard Zins <rizins@zins.dynu.com>
2020-04-29 12:39:40 -0700

    simplified my code a lot. Turns out there is an easier way to do this with state based rendering. I unfortunately cannot make the state change back to the original yet though.

--- AMEL/AMEL/Views/ButtonView.swift
+++ AMEL/AMEL/Views/ButtonView.swift
@@ -8,18 +8,8 @@
 import SwiftUI
 import UIKit
 
-func displayToast(controller: UIViewController, message: String, seconds: Double){
-    let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
-    alert.view.backgroundColor = UIColor.black
-    alert.view.alpha = 0.6
-    alert.view.layer.cornerRadius = 15
-    controller.present(alert, animated: true)
-    DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + seconds){
-        alert.dismiss(animated: true)
-    }
-}
-
 struct ButtonView: View {
+    @State private var eventRecorded = false
     @ObservedObject private var locationManager = LocationManager()
     @Environment(\.managedObjectContext) var managedObjectContext
     private var name:String
@@ -28,25 +18,35 @@ struct ButtonView: View {
         self.name = name
         self.color = color
     }
-    
     var body: some View {
         Button(action: {
 			ButtonAction.record(self.name, self.color, self.locationManager, self.managedObjectContext)
 			let newEvent:Event = ButtonAction.createEvent(self.managedObjectContext)
             if(ButtonAction.saveEvent(newEvent, self.name, self.color, self.managedObjectContext)){
-                let cont = UIViewController()
-                displayToast(controller: cont, message: "Event Recorded!", seconds: 3)
+                self.eventRecorded = true
+            } else {
+                self.eventRecorded = false
             }
         }){
-            Text(self.name)
-            .frame(minWidth: 0, maxWidth: .infinity)
-            .frame(minHeight: 0, maxHeight: .infinity)
-            .font(.largeTitle)
-            .padding(10)
-            .foregroundColor(.primary)
-            .background(Color(self.color))
-            .cornerRadius(40)
-            
+            if(self.eventRecorded){
+                Text("Event Recorded")
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .frame(minHeight: 0, maxHeight: .infinity)
+                .font(.largeTitle)
+                .padding(10)
+                .foregroundColor(.primary)
+                .background(Color(self.color))
+                .cornerRadius(40)
+            } else {
+                Text(self.name)
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .frame(minHeight: 0, maxHeight: .infinity)
+                .font(.largeTitle)
+                .padding(10)
+                .foregroundColor(.primary)
+                .background(Color(self.color))
+                .cornerRadius(40)
+            }
         }
     }
 }
