5455edcf192157fe640fc1531f03535d4719d2c1
Tristan Nibbe <tristan.nibbe114@myci.csuci.edu>
2020-04-29 20:14:09 -0700

    Added protocol and added reference point Information to core data model of event

---
+++ AMEL/AMEL/Models/ReferencePointViewModelProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  ReferencePointViewModelProtocol.swift
+//  AMEL
+//
+//  Created by Tristan Nibbe on 4/29/20.
+//  Copyright © 2020 Marcellini, Neil. All rights reserved.
+//
+
+import Foundation
+
+public protocol ReferencePointViewModelProtocol{
+    func getReferencPointHeading() -> String
+    func getReferencePointDistance() -> String
+
+}
