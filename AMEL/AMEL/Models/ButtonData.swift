103cda50a7aad29a8cabc9b7e97efebb3ac65973
Jeffrey Romero <jeffreybrianr@yahoo.com>
2020-05-04 13:52:05 -0700

    Reverted changes. Color updates properly with picker.

--- AMEL/AMEL/Models/ButtonData.swift
+++ AMEL/AMEL/Models/ButtonData.swift
@@ -5,7 +5,12 @@ import SwiftUI
 
 class ButtonData: ObservableObject {
     @Published var name: String
-	@Published var color: Int = 0
+	@Published var color: Int = 0 {
+		didSet {
+			print("updated")
+			self.updated = true
+		}
+	}
     @Published var updated = false
     init(button: CustomButton) {
         let colors = Colors().colorNames
