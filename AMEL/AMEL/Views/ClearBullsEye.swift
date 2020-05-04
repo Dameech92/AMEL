e0b040b2839e8b40daf8687573b97976bbbd9dd4
R Dimitri Cognata <roger.cognata.560.myci.csuci.edu>
2020-05-04 12:50:51 -0700

    unfunshed changes for deleteBullsEye

---
+++ AMEL/AMEL/Views/ClearBullsEye.swift
@@ -0,0 +1,21 @@
+//
+//  ClearBullsEye.swift
+//  AMEL
+//
+//  Created by Dimitri Cognata on 4/29/20.
+//  Copyright © 2020 Marcellini, Neil. All rights reserved.
+//
+
+import SwiftUI
+
+struct ClearBullsEye: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ClearBullsEye_Previews: PreviewProvider {
+    static var previews: some View {
+        ClearBullsEye()
+    }
+}
