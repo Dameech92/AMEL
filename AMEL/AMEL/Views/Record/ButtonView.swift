18c056dc05dbd98f48e7af24ad5fb68bc4b08dd7
neil-marcellini <neil.marcellini@gmail.com>
2020-06-07 14:59:41 -0700

    Made button name font smaller on small screens.

--- AMEL/AMEL/Views/Record/ButtonView.swift
+++ AMEL/AMEL/Views/Record/ButtonView.swift
@@ -11,16 +11,17 @@ struct ButtonView: View {
     @ObservedObject private var locationManager = LocationManager()
     @Environment(\.managedObjectContext) var managedObjectContext
     @State private var buttonText = ""
-    
+    @Environment(\.verticalSizeClass) var heightSizeClass
+    @Environment(\.horizontalSizeClass) var widthSizeClass
     private var name:String
     private var color:String
     init(name:String, color: String) {
         self.name = name
 		self.color = color
     }
-    
     var body: some View {
-        Button(action: {
+        let smallText = heightSizeClass == .compact || widthSizeClass == .compact
+        return Button(action: {
             ButtonAction.record(self.name, self.color, self.locationManager, self.managedObjectContext)
 			let newEvent:Event = ButtonAction.createEvent(self.managedObjectContext)
             ButtonAction.saveEvent(newEvent, self.name, self.color, self.managedObjectContext)
@@ -30,7 +31,7 @@ struct ButtonView: View {
             }
             .frame(minWidth: 0, maxWidth: .infinity)
             .frame(minHeight: 0, maxHeight: .infinity)
-            .font(.largeTitle)
+            .font(smallText ? .body : .largeTitle)
             .padding(10)
             .foregroundColor(.black)
             .background(Color(self.color))
