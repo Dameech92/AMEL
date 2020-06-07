86f5a6eaa3ec9095c90c28a44a1d92151a873897
neil-marcellini <neil.marcellini@gmail.com>
2020-06-07 14:54:52 -0700

    Buttons on record page layout better in landscape mode.

--- AMEL/AMEL/Views/Record/RecordView.swift
+++ AMEL/AMEL/Views/Record/RecordView.swift
@@ -11,33 +11,49 @@ struct RecordView: View {
     @FetchRequest(fetchRequest: CustomButton.getCustomButton()) var customButton:FetchedResults<CustomButton>
     @FetchRequest(fetchRequest: ReferencePoint.getPoints()) var points:FetchedResults<ReferencePoint>
     @Environment(\.managedObjectContext) var managedObjectContext
-    
+    @Environment(\.verticalSizeClass) var heightSizeClass
 	private let locationVM = LocationViewModel()
 	private let headingVM = HeadingViewModel()
     let refPointVM = ActiveRefPointVM()
 	
 	var body: some View {
         ActiveRefPointVM.shared.executeFetchRequest(points: points)
-
+        let landscapeLayout = heightSizeClass == .compact
 		return ZStack {
 			Color("stealth").edgesIgnoringSafeArea(.all)
 			VStack {
                 LiveDataView()
                 Divider()
-				HStack {
-					VStack {
-                        ForEach(0 ..< self.customButton.count/2) {
-                            ButtonView(name: self.customButton[$0].buttonName!, color: self.customButton[$0].buttonColor!)
-						}.padding(10)
-					}
+                if landscapeLayout {
                     VStack {
-                        ForEach(self.customButton.count/2 ..< self.customButton.count) {
-                            ButtonView(name: self.customButton[$0].buttonName!, color: self.customButton[$0].buttonColor!)
-                        }.padding(10)
+                        HStack {
+                            ForEach(0 ..< self.customButton.count/2) {
+                                ButtonView(name: self.customButton[$0].buttonName!, color: self.customButton[$0].buttonColor!)
+                            }
+                        }.padding(2)
+                        HStack {
+                            ForEach(self.customButton.count/2 ..< self.customButton.count) {
+                                ButtonView(name: self.customButton[$0].buttonName!, color: self.customButton[$0].buttonColor!)
+                            }
+                        }.padding(2)
+                    }
+                } else {
+                    HStack {
+                        VStack {
+                            ForEach(0 ..< self.customButton.count/2) {
+                                ButtonView(name: self.customButton[$0].buttonName!, color: self.customButton[$0].buttonColor!)
+                            }.padding(10)
+                        }
+                        VStack {
+                            ForEach(self.customButton.count/2 ..< self.customButton.count) {
+                                ButtonView(name: self.customButton[$0].buttonName!, color: self.customButton[$0].buttonColor!)
+                            }.padding(10)
+                        }
                     }
                 }
+				
 			}
-			.padding(10.0)
+			.padding(10)
 		}
 	}
 }
