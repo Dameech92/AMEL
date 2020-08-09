51d477c58b0084f2cdee41c519b1e324038240d1
neil-marcellini <neil.marcellini@gmail.com>
2020-08-09 13:25:11 -0700

    Made entire tutorial card swipe-able

--- AMEL/AMEL/Views/Tutorial/TutorialCard.swift
+++ AMEL/AMEL/Views/Tutorial/TutorialCard.swift
@@ -13,26 +13,32 @@ struct TutorialCard: View {
     let text: String
     let imageName: String
     var body: some View {
-        VStack {
-            HStack {
-                Text(name)
-                    .font(.system(size: 50.0))
-                    .bold()
+        ZStack {
+            Color(.systemBackground) //makes whole view draggable
+            VStack {
+                HStack {
+                    Text(name)
+                        .font(.system(size: 50.0))
+                        .bold()
+                        .padding()
+                    Spacer()
+                }
+                HStack {
+                    Text(self.text)
+                        .font(.title)
+                        .lineLimit(nil)
+                        .padding()
+                    Spacer()
+                }
+                
+                Image(self.imageName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
                     .padding()
                 Spacer()
             }
-            
-            Text(self.text)
-                .font(.title)
-                .lineLimit(nil)
-                .padding()
-            Image(self.imageName)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .padding()
+            .padding()
         }
-        .padding()
-        
     }
 }
 
@@ -40,6 +46,5 @@ struct TutorialCard_Previews: PreviewProvider {
     static var previews: some View {
         TutorialCard(name: "Record", text: "This is the main page where you can record events and see real time data.", imageName: "record")
         TutorialCard(name: "Welcome to AMEL!", text: "Since this is your first time here, how about a quick tutorial?\nClick the arrow or swipe left to get started!\nYou may end the tutorial at any time.", imageName: "launch_logo")
-            .previewDevice("iPhone 11")
     }
 }
