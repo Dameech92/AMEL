1c8cda198f4829ef042836dc7ce9804c687bb83f
neil-marcellini <neil.marcellini@gmail.com>
2020-08-16 17:10:01 -0700

    Made entire color selection row clickable.

--- AMEL/AMEL/Views/Settings/ColorDetail.swift
+++ AMEL/AMEL/Views/Settings/ColorDetail.swift
@@ -16,14 +16,17 @@ struct ColorDetail: View {
     let colorNames = Colors().colorNames
     let button: CustomButton
     var body: some View {
-        HStack {
-            Image(systemName: self.color == self.colorNames[self.buttonData.color] ? "circle.fill": "circle")
-            Text(self.color)
-            Spacer()
-            Rectangle()
-                .fill(Color(self.color))
-                .frame(width:40, height: 40)
-                .cornerRadius(5)
+        ZStack {
+            Color(.systemBackground) //makes whole view draggable
+            HStack {
+                Image(systemName: self.color == self.colorNames[self.buttonData.color] ? "circle.fill": "circle")
+                Text(self.color)
+                Spacer()
+                Rectangle()
+                    .fill(Color(self.color))
+                    .frame(width:40, height: 40)
+                    .cornerRadius(5)
+            }
         }.onTapGesture {
             self.viewModel.updateButton(name: self.buttonData.name, color: Colors().colorToInt(color: self.color), button: self.button)
         }.font(.largeTitle)
